0c30be41785e1b8764853d7f08ec9194bf3d7f60
Marak Squires <marak.squires@gmail.com>
2012-04-09 02:20:45 -0700

    [examples] Added examples for managing Keys

---
+++ node.js/examples/list-keys.js
@@ -0,0 +1,17 @@
+var nj = require('../lib/client'),
+    fs = require('fs');
+
+var client = nj.createClient({
+      username: 'jim',
+      password: '1234',
+      remoteUri: 'http://localhost:9001',
+      debug: true
+    });
+
+client.keys.list('jim', function(err, result){
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(JSON.stringify(result, null, 2, true));
+});
