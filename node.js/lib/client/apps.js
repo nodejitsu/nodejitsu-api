40b75e4c6ca1c43e354e415f9fd815a1a5b4f07d
3rd-Eden <info@3rd-Eden.com>
2013-01-07 16:41:31 +0100

    [datacenter] Make the request method datacenter aware [minor] Removed the `res.statusCode` code smell
    Applications that are hosted in different datacenters need to be called at their
    correct endpoint. The cloud details are inside of the app documents so we need to fetch
    both the endpoint resource and the app document before we know which datacenter we
    need to hit for the given change.
    
    The statusCode was introduced in commit 9cea32719a797bbd769eaa89ea7cfab85c7f345d but
    Avian has no memory of why he did it. So i'm removing it to prevent possible breakage
    when you expect that the result is always an object.

--- node.js/lib/client/apps.js
+++ node.js/lib/client/apps.js
@@ -18,11 +18,82 @@ var util = require('util'),
 // with Nodejitsu's Apps API
 //
 var Apps = exports.Apps = function (options) {
+  this.clouds = {};
+  this.datacenters = {};
+
   Client.call(this, options);
 };
 
 // Inherit from Client base object
 util.inherits(Apps, Client);
+
+//
+// ### @private function request
+// #### @options {Object}
+// #### @callback {function} Continuation to call if errors occur.
+// Overrites the default request logic to make it aware of datacenter locations
+//
+Apps.prototype.request = function (options, callback) {
+  var self = this,
+      flow = [];
+
+  // we don't need to have any datacenter information for these types of calls
+  if (options.remoteUri || !options.app || !options.method || options.method === 'GET') {
+    return Client.prototype.request.apply(this, arguments);
+  }
+
+  //
+  // We don't have any datacenter data by default as it's only needed for
+  // starting or stopping the application.
+  //
+  if (!Object.keys(this.datacenters)) flow.push(function (done) {
+    self.endpoints(function endpoints(err, datacenters) {
+      if (err) return done(err);
+
+      self.datacenters = datacenters;
+      done();
+    });
+  });
+
+  //
+  // Make sure that we have this app in our cloud cache so we know in which
+  // datacenter it is.
+  //
+  if (!(options.app in this.clouds)) flow.push(function (done) {
+    self.list(function apps(err, applications) {
+      if (err) return done(err);
+
+      self.clouds = applications.reduce(function reduce(memo, app) {
+        memo[app] = app.cloud;
+      }, {});
+
+      done();
+    });
+  });
+
+  //
+  // Upgrade the remoteUri to the correct location of the datacenter or we will
+  // not be able to communicate correctly with it.
+  //
+  flow.push(function (done) {
+    var cloud = self.clouds[options.app];
+
+    options.remoteUri = self.datacenters[cloud.provider][cloud.datacenter];
+    Client.prototype.request.call(self, options, callback);
+  });
+
+  var completed = 0;
+  (function iterate() {
+    flow[completed](function next(err) {
+      if (err) {
+        callback(err);
+      } else {
+        completed++;
+        if (completed !== flow.length) iterate();
+      }
+    });
+  }());
+};
 
 //
 // ### function list (username, callback)
@@ -38,7 +109,7 @@ Apps.prototype.list = function (username, callback) {
   this.request({ uri: ['apps', username] }, function (err, result, res) {
     if (err) return callback(err);
 
-    callback(err, result.apps || res.statusCode);
+    callback(err, result.apps);
   });
 };
 
@@ -51,11 +122,7 @@ Apps.prototype.list = function (username, callback) {
 Apps.prototype.create = function (app, callback) {
   var appName = defaultUser.call(this, app.name);
 
-  this.request({ method: 'POST', uri: ['apps', appName], body: app }, function (err, result, res) {
-    if (err) return callback(err);
-
-    callback(null, result || res.statusCode);
-  });
+  this.request({ method: 'POST', uri: ['apps', appName], body: app }, callback);
 };
 
 //
@@ -71,7 +138,7 @@ Apps.prototype.view = function (appName, callback) {
   this.request({ uri: argv }, function (err, result, res) {
     if (err) return callback(err);
 
-    callback(err, result.app || res.statusCode);
+    callback(err, result.app);
   });
 };
 
@@ -86,11 +153,7 @@ Apps.prototype.update = function (appName, attrs, callback) {
   appName = defaultUser.call(this, appName);
   var argv = ['apps'].concat(appName.split('/'));
 
-  this.request({ method: 'PUT', uri: argv, body: attrs }, function (err, result, res) {
-    if (err) return callback(err);
-
-    callback(err, result || res.statusCode);
-  });
+  this.request({ method: 'PUT', uri: argv, body: attrs }, callback);
 };
 
 //
@@ -103,11 +166,7 @@ Apps.prototype.destroy = function (appName, callback) {
   appName = defaultUser.call(this, appName);
   var argv = ['apps'].concat(appName.split('/'));
 
-  this.request({ method: 'DELETE', uri: argv }, function (err, result, res) {
-    if (err) return callback(err);
-
-    callback(err, result || res.statusCode);
-  });
+  this.request({ method: 'DELETE', uri: argv }, callback);
 };
 
 //
@@ -120,11 +179,7 @@ Apps.prototype.start = function (appName, callback) {
   appName = defaultUser.call(this, appName);
   var argv = ['apps'].concat(appName.split('/')).concat('start');
 
-  this.request({ method: 'POST', uri: argv }, function (err, result, res) {
-    if (err) return callback(err);
-
-    callback(err, result || res.statusCode);
-  });
+  this.request({ method: 'POST', uri: argv }, callback);
 };
 
 //
@@ -137,11 +192,7 @@ Apps.prototype.restart = function (appName, callback) {
   appName = defaultUser.call(this, appName);
   var argv = ['apps'].concat(appName.split('/')).concat('restart');
 
-  this.request({ method: 'POST', uri: argv }, function (err, result, res) {
-    if (err) return callback(err);
-
-    callback(err, result || res.statusCode);
-  });
+  this.request({ method: 'POST', uri: argv }, callback);
 };
 
 //
@@ -154,11 +205,7 @@ Apps.prototype.stop = function (appName, callback) {
   appName = defaultUser.call(this, appName);
   var argv = ['apps'].concat(appName.split('/')).concat('stop');
 
-  this.request({ method: 'POST', uri: argv }, function (err, result, res) {
-    if (err) return callback(err);
-
-    callback(err, result || res.statusCode);
-  });
+  this.request({ method: 'POST', uri: argv }, callback);
 };
 
 //
@@ -172,11 +219,7 @@ Apps.prototype.available = function (app, callback) {
   var appName = defaultUser.call(this, app.name),
       argv = ['apps'].concat(appName.split('/')).concat('available');
 
-  this.request({ method: 'POST', uri: argv, body: app }, function (err, result, res) {
-    if (err) return callback(err);
-
-    callback(err, result || res.statusCode);
-  });
+  this.request({ method: 'POST', uri: argv, body: app }, callback);
 };
 
 //
@@ -188,11 +231,36 @@ Apps.prototype.available = function (app, callback) {
 //
 Apps.prototype.setDrones = function (appName, drones, callback) {
   appName = defaultUser.call(this, appName);
-  var argv = ['apps'].concat(appName.split('/')).concat('drones');
+  var argv = ['apps'].concat(appName.split('/')).concat('cloud');
 
-  this.request({ method: 'POST', uri: argv, body: { drones: drones }}, function (err, result, res) {
+  this.request({ method: 'POST', uri: argv, body: [{ drones: drones }] }, callback);
+};
+
+//
+// ### function datacenter(appName, cloud, callback)
+// #### @appName {String} Name of the application
+// #### @cloud {Object} Cloud specification
+// #### @callback {Function} Continuation to pass control to when complete
+// Deploy the given application in a new datacenter.
+//
+Apps.prototype.datacenter = function (appName, cloud, callback) {
+  appName = defaultUser.call(this, appName);
+  var argv = ['apps'].concat(appName.split('/')).concat('cloud');
+
+  if (!Array.isArray(cloud)) cloud = [cloud];
+
+  this.request({ method: 'POST', uri: argv, body: cloud }, callback);
+};
+
+//
+// ### function endpoints(callback)
+// #### @callback {function} Continuation to respond to when complete.
+// Retrieves a list of currenlty active datacenters and providers
+//
+Apps.prototype.endpoints = function (callback) {
+  this.request({ uri: ['endpoints'] }, function (err, result) {
     if (err) return callback(err);
 
-    callback(err, result || res.statusCode);
+    callback(err, result.endpoints);
   });
 };
